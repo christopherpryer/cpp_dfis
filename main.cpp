08719228115aa40d80b7cfc984dca0ddb0fa3265
Chris Pryer <christophpryer@gmail.com>
2019-10-20 00:36:12 -0400

    added read_csv and edit functions

--- main.cpp
+++ main.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
 #include <vector>
-#include <string>
-
 using namespace std;
+
+using vec = vector<string>;
+using matrix = vector<vec>;
+
+matrix readCSV(string filename)
+{
+   char separator = ',';
+   matrix result;
+   string row, item;
+
+   ifstream in(filename);
+   while(getline(in,row))
+   {
+      vec R;
+      stringstream ss(row);
+      while (getline(ss, item, separator)) R.push_back(item);
+      result.push_back(R);
+   }
+   in.close();
+   return result;
+}
+
+void printMatrix(const matrix &M)
+{
+   for (vec row : M)
+   {
+      for (string s: row) cout << setw(12) << left << s << " ";
+      cout << '\n';
+   }
+}
+
+void deleteRow(matrix &M, int row)
+{
+   if (row < M.size()) M.erase(M.begin() + row);
+}
+
+void deleteCol(matrix &M, int col)
+{
+   for (vec &row: M)
+   {
+      if (col < row.size()) row.erase(row.begin() + col);
+   }
+}
+
+void edit(matrix &M, int i, int j, string value)
+{
+   if (i < M.size() && j < M[i].size()) M[i][j] = value;
+}
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+   matrix data = readCSV("test_data.csv");
+   deleteCol(data, 0);
+   printMatrix(data);
 }
